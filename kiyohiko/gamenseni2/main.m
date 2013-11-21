d7f6f31e7b81b2b8320e53115e013d4a55c4bfde
clear <e125724@Kiyohiko.local>
2013-11-21 11:28:24 +0900

    second commit

---
+++ kiyohiko/gamenseni2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gamenseni2
+//
+//  Created by clear  on 2013/11/21.
+//  Copyright (c) 2013年 Kiyohiko.Iwamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
