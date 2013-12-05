a12601f5818dfab2aa885c3ec3848dd8e82c6d88
Shin <e125752@w236-108.dyn.u-ryukyu.ac.jp>
2013-12-05 13:03:06 +0900

    12/5 re_add Master Projects

---
+++ one-stroke_sketch/one-stroke_sketch/Line.m
@@ -0,0 +1,13 @@
+//
+//  Line.m
+//  one-stroke_sketch
+//
+//  Created by Shin,ichi  Uehara on 2013/12/04.
+//  Copyright (c) 2013年 Shin,ichi  Uehara. All rights reserved.
+//
+
+#import "Line.h"
+
+@implementation Line
+
+@end
